599b0931cc1236e71a0080397b1dbc0be65bdc1c
Konstantin Arakantsev <kostya3312@gmail.com>
2020-04-20 22:33:57 +0300

    incapsulate tests by testing framework

--- simple.js
+++ simple.js
@@ -1,13 +1,27 @@
-const sum = (a, b) => a + b
+const sum = (a, b) => a - b
 const subtract = (a, b) => a - b
 
-let result = sum(7, 3)
-let expected = 10
-expect(result).toBe(expected)
+test('7 + 3 = 10', () => {
+  let result = sum(7, 3)
+  let expected = 10
+  expect(result).toBe(expected)
+})
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+test('7 - 3 = 4', () => {
+  result = subtract(7, 3)
+  expected = 4
+  expect(result).toBe(expected)
+})
+
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`🗸 ${title}`)
+  } catch (error) {
+    console.error(error)
+    console.error(`╳ ${title}`)
+  }
+}
 
 function expect(actual) {
   return {
