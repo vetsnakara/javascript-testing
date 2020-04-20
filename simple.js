7ce131b27b664d2a45f549884bc65fb96c663e9d
Konstantin Arakantsev <kostya3312@gmail.com>
2020-04-20 22:24:23 +0300

    abstract test assertions

--- simple.js
+++ simple.js
@@ -1,14 +1,20 @@
-const sum = (a, b) => a - b
+const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
 
 let result = sum(7, 3)
 let expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    }
+  }
 }
